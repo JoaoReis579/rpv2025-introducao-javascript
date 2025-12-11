13f8ed358790dddb275f85cc828e9fb9aab288c0
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-12-11 20:44:23 -0300

    feat: updating DOM

--- manipulando-a-DOM/script.js
+++ manipulando-a-DOM/script.js
@@ -1,3 +1,21 @@
-const paragrafo = document.createElement('p')
+const mainElement = document.createElement('main')
+const h1Element = document.createElement('h1')
+const pElement = document.createElement('p')
+const spanElement = document.createElement('span')
 
-console.log(paragrafo)
+const bodyElement = document.querySelector('body')
+
+h1Element.innerHTML = "Daniel Ventura de Almeida"
+pElement.innerHTML = "Desenvolvedor"
+spanElement.innerHTML = "Cataguases - MG"
+
+mainElement.classList.add('flex', 'flex-col', 'gap-4')
+
+h1Element.style.fontSize = '2.4rem'
+pElement.style.fontSize = '1.8rem'
+spanElement.style.fontSize = '1.4rem'
+
+bodyElement.appendChild(mainElement)
+mainElement.appendChild(h1Element)
+mainElement.appendChild(pElement)
+mainElement.appendChild(spanElement)
